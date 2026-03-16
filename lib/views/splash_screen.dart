a8464e6ddb08d968de800082c902a6a20b94011a
Bilal Saeed <mianzainjh@gmail.com>
2026-03-16 10:39:14 +0500

    Today I learned how to animate a pic. I learned about animation. And make a splash screen for my covid app.

---
+++ lib/views/splash_screen.dart
@@ -0,0 +1,89 @@
+
+
+import 'package:covid19_app/views/world_states.dart';
+import 'package:flutter/material.dart';
+import 'dart:math'as math;
+import 'dart:async';
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin{
+
+  late final AnimationController _controller = AnimationController(
+      duration: const Duration(seconds: 3),
+      vsync: this
+  )..repeat();
+
+  void dispose(){
+    
+    super.dispose();
+    _controller.dispose();
+
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Timer(
+      const Duration(seconds: 5),
+          () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const WorldStatesScreen(),
+        ),
+      ),
+    );
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children:  [
+            AnimatedBuilder(
+                animation: _controller,
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  child: const Center(
+                    child: Image(image: AssetImage('assets/images/virus.png')),
+                  ),
+                ),
+                builder: (BuildContext context, Widget? child){
+                  return Transform.rotate(
+                      angle: _controller.value * 2.0 * math.pi,
+                          child: child,
+                  );
+                }
+            ),
+
+            SizedBox(height: MediaQuery.of(context).size.height * 0.08,),
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                  "Covid-19\nTracker App",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.height * 0.04,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
